800cf47b0249063a3e3bbd29011db5a184e33ae8
NayanN91 <nayan.nanouche@gmail.com>
2023-06-17 14:14:55 +0200

    fin du code du jeu et debut du css

--- src/App.tsx
+++ src/App.tsx
@@ -5,124 +5,122 @@ import { useState } from 'react';
 import { useRef } from 'react';
 import logoFraise from "./assets/png-transparent-strawberry-illustration-ms-pac-man-galaxian-super-smash-bros-for-nintendo-3ds-and-wii-u-arcade-game-pac-man-super-smash-bros-for-nintendo-3ds-and-wii-u-text-rectangle.png"
 import gameScreen from "./assets/56455_old-computer-monitor-png-removebg-preview.png"
-import useInterval from './useInterval';
-import {getContext} from
+import { useInterval } from './useInterval';
 
-const canvasX = 1000
-const canvasY = 1000
-const initialSnake = [[4,10],[4,10]]
-const initialApple = [14,10]
-const scale = 50
-const timeDelay = 100
+const canvasX = 1000;
+const canvasY = 1000;
+const initialSnake = [[4, 10], [4, 10]];
+const initialApple = [14, 10];
+const scale = 50;
+const timeDelay = 100;
 
-function App() { 
-  const canvasRef = useRef(null)
-  const [snake, setSnake] = useState(initialSnake)
-  const [apple, setApple] = useState(initialApple)
-  const [direction, setDirection] = useState([0, -1])
-  const [delay, setDelay] = useState<number | null>(null)
-  const [gameOver, setGameOver] = useState(false)
-  const [score, setScore] = useState(0)
+function App() {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [snake, setSnake] = useState(initialSnake);
+  const [apple, setApple] = useState(initialApple);
+  const [direction, setDirection] = useState([0, -1]);
+  const [delay, setDelay] = useState<number | null>(null);
+  const [gameOver, setGameOver] = useState(false);
+  const [score, setScore] = useState(0);
 
-  useInterval(() => runGame(), delay)
-  useEffect(
-		() => {
-			let fruit = document.getElementById("fruit") as HTMLCanvasElement
-			if (canvasRef.current) {
-				const canvas = canvasRef.current
-				const ctx = canvas.getContext("2d")
-				if (ctx) {
-					ctx.setTransform(scale, 0, 0, scale, 0, 0)
-					ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
-					ctx.fillStyle = "#a3d001"
-					snake.forEach(([ x, y ]) => ctx.fillRect(x, y, 1, 1))
-					ctx.drawImage(fruit, apple[0], apple[1], 1, 1)
+  useInterval(() => runGame(), delay);
+
+  useEffect(() => {
+    let fruit = document.getElementById("fruit") as HTMLImageElement;
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        ctx.setTransform(scale, 0, 0, scale, 0, 0);
+        ctx.clearRect(0, 0, canvasX, canvasY);
+        ctx.fillStyle = "#a3d001";
+        snake.forEach(([x, y]) => ctx.fillRect(x, y, 1, 1));
+        ctx.drawImage(fruit, apple[0], apple[1], 1, 1);
       }
     }
+  }, [snake, apple, gameOver]);
 
-  })
+  function handleSetScore() {
+    if (score > Number(localStorage.getItem("snakeScore"))) {
+      localStorage.setItem("snakeScore", JSON.stringify(score));
+    }
+  }
 
   function play() {
-    setSnake(initialSnake)
-    setApple(initialApple)
-    setDirection([1,0])
-    setDelay(timeDelay)
-    setScore(0)
-    setGameOver(false)
+    setSnake(initialSnake);
+    setApple(initialApple);
+    setDirection([1, 0]);
+    setDelay(timeDelay);
+    setScore(0);
+    setGameOver(false);
   }
 
   function checkCollision(head: number[]) {
-		for (let i = 0; i < head.length; i++) {
-			if (head[i] < 0 || head[i] * scale >= canvasX) return true
-		}
-		for (const s of snake) {
-			if (head[0] === s[0] && head[1] === s[1]) return true
-		}
-  return false 
+    for (let i = 0; i < head.length; i++) {
+      if (head[i] < 0 || head[i] * scale >= canvasX) return true;
+    }
+    for (const s of snake) {
+      if (head[0] === s[0] && head[1] === s[1]) return true;
+    }
+    return false;
   }
 
-  function appleAte (newSnake: number[][]) {
-    let coord = apple.map(() => Math.floor(Math.random() * canvasX / scale))
+  function appleAte(newSnake: number[][]) {
+    let coord = apple.map(() => Math.floor(Math.random() * (canvasX / scale)));
     if (newSnake[0][0] === apple[0] && newSnake[0][1] === apple[1]) {
-			let newApple = coord
-			setScore(score + 1)
-			setApple(newApple)
-			return true
-		}
-		return false
-
+      let newApple = coord;
+      setScore(score + 1);
+      setApple(newApple);
+      return true;
+    }
+    return false;
   }
 
   function runGame() {
-
-    const newSnake = [...snake]
-    const newSnakeHead = [newSnake[0][0] + direction[0], newSnake[0][1] + direction[1]]
-    newSnake.unshift(newSnakeHead)
-    if(checkCollision(newSnakeHead)) {
-      setDelay(null)
-      setGameOver(true)
-      handleSetScore()
+    const newSnake = [...snake];
+    const newSnakeHead = [newSnake[0][0] + direction[0], newSnake[0][1] + direction[1]];
+    newSnake.unshift(newSnakeHead);
+    if (checkCollision(newSnakeHead)) {
+      setDelay(null);
+      setGameOver(true);
+      handleSetScore();
     }
-    if(!appleAte(newSnake)) {
-      newSnake.pop()
+    if (!appleAte(newSnake)) {
+      newSnake.pop();
     }
-    setSnake(newSnake)
+    setSnake(newSnake);
   }
 
   function changeDirection(e: React.KeyboardEvent<HTMLDivElement>) {
     switch (e.key) {
       case "ArrowLeft":
-        setDirection([-1, 0])
-        break
-        case "ArrowUp":
-        setDirection([0, -1])
-        break
-        case "ArrowRight":
-        setDirection([1, 0])
-        break
-        case "ArrowDown":
-        setDirection([0, 1])
-        break
+        setDirection([-1, 0]);
+        break;
+      case "ArrowUp":
+        setDirection([0, -1]);
+        break;
+      case "ArrowRight":
+        setDirection([1, 0]);
+        break;
+      case "ArrowDown":
+        setDirection([0, 1]);
+        break;
     }
   }
 
-
-
-
-
-
   return (
-<div onKeyDown={(e) => changeDirection(e)} >
-  <img id='fruit' src={logoFraise} alt="fruit" width="30" />
-  <img src={gameScreen} alt="screen" width="30" />
-  <canvas className='playArea' ref={canvasRef} width={'${canvasX}px'} height={'${canvasY}px'} />
-  {gameOver && <div className='gameOver'>Game Over</div>}
-  <button onClick={play} className='playButton'>Play</button>
-  <div className='scoreBox'>
-    <h2>Score: {score}</h2>
-    <h2>High Score: {localStorage.getItem("snakeScore")}</h2>
-  </div>
-</div>
-  
-  )}
+    <div onKeyDown={(e) => changeDirection(e)}>
+      <img id='fruit' src={logoFraise} alt="fruit" width="30" />
+      <img src={gameScreen} alt="screen" width="4000" className='screen' />
+      <canvas className='playArea' ref={canvasRef} width={`${canvasX}px`} height={`${canvasY}px`} />
+      {gameOver && <div className='gameOver'>Game Over</div>}
+      <button onClick={play} className='playButton'>Play</button>
+      <div className='scoreBox'>
+        <h2>Score: {score}</h2>
+        <h2>High Score: {localStorage.getItem("snakeScore")}</h2>
+      </div>
+    </div>
+  );
+}
+
 export default App;
